ce7acb163e8df7477857da9c8b3ac02c66b27648
Mathieu Clement <tiktaktok@users.noreply.github.com>
2020-10-24 12:11:49 -0700

    Implement calendar and test

---
+++ src/calendar.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import absolute_filename
+
+import configparser
+import icalendar
+
+class Calendar:
+
+    def __init__(self, schedules):
+        if schedules is None or schedules == []:
+            raise ValueError('Calendar cannot be empty')
+
+        self.settings = self.Settings()
+        self.principal_cfi = self.settings.principal_cfi
+        self.fbo_url = self.settings.fbo_url
+        self.fbo_address = self.settings.fbo_address
+
+        self.schedules = schedules
+        self.cal = icalendar.Calendar()
+        self.cal['version'] = '2.0'
+        self.cal['prodid'] = '-//TIKTAKTOK//PAPERLESS-ICAL//EN'
+
+        for schedule in schedules:
+            self.add_schedule(schedule)
+
+
+    def write_filename(self, path=absolute_filename('calendar.ics')):
+        with open(path, 'wb') as f:
+            self.write_file(f)
+
+
+    def write_file(self, f):
+        f.write(self.cal.to_ical())
+
+
+    def add_schedule(self, schedule):
+        self.cal.add_component(self.event(schedule))
+
+
+    def event(self, schedule):
+        event = icalendar.Event()
+        event.add('uid', 'PAPERLESS-ICAL-EVENT-' + schedule.id)
+        event.add('dtstart', schedule.start_dt)
+        event.add('dtend', schedule.end_dt)
+        event.add('summary', self.summary(schedule))
+        event.add('categories', self.categories(schedule), encode=0)
+        event.add('description','PaperlessFBO: https://{}/'
+                                .format(self.fbo_url))
+        event.add('location', self.fbo_address)
+        event.add('status', 'CONFIRMED')
+
+        return event
+
+
+    def summary(self, schedule):
+        if self.is_flight(schedule):
+            summary = 'Flight {}'.format(schedule.tail_number)
+            if schedule.cfi != self.principal_cfi:
+                summary = summary + ' ({})'.format(schedule.cfi)
+        else:
+            summary = schedule.tail_number
+        return summary
+
+
+    def categories(self, schedule):
+        if self.is_flight(schedule):
+            return 'FLIGHT'
+        else:
+            return schedule.tail_number
+        
+
+
+    def is_flight(self, schedule):
+        return schedule.tail_number.startswith('N')
+
+
+    class Settings:
+        def __init__(self):
+            config = configparser.ConfigParser()
+            settings_file_path = absolute_filename('settings.ini')
+            config.read(settings_file_path)
+            self.principal_cfi = config['calendar']['principal_cfi']
+            self.fbo_url = config['calendar']['fbo_url']
+            self.fbo_address = config['calendar']['fbo_address']
--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -4,6 +4,7 @@
 import src as paperless
 
 from datetime import datetime as dt
+import pdb
 import re
 import tempfile
 from unittest import TestCase
@@ -12,32 +13,46 @@ class TestCalendar(TestCase):
 
     def setUp(self):
         self.schedules = [
-"Schedule('124926','N12345',dt(2020, 10, 21, 14, 0),dt(2020, 10, 21, 16, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('125806','N12345',dt(2020, 10, 24, 14, 0),dt(2020, 10, 24, 16, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('332807','N12345',dt(2020, 10, 25, 16, 0),dt(2020, 10, 25, 18, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('123986','N12345',dt(2020, 10, 30, 14, 0),dt(2020, 10, 30, 16, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('123988','N9876A',dt(2020, 11, 7, 11, 0),dt(2020, 11, 7, 13, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('123989','N12345',dt(2020, 11, 11, 11, 0),dt(2020, 11, 11, 13, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('123991','N12345',dt(2020, 11, 14, 11, 0),dt(2020, 11, 14, 13, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('123992','N12345',dt(2020, 11, 21, 11, 0),dt(2020, 11, 21, 13, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('123993','N12345',dt(2020, 11, 22, 11, 0),dt(2020, 11, 22, 13, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('124311','N12345',dt(2020, 11, 28, 14, 0),dt(2020, 11, 28, 16, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)", 
-"Schedule('124314','N12345',dt(2020, 11, 29, 14, 0),dt(2020, 11, 29, 16, 0),\
-'Clement, Mathieu','CfiLastName, CfiFirstName',None)"]
+paperless.Schedule('124926','N12345',
+dt(2020, 10, 21, 14, 0), dt(2020, 10, 21, 16, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('125806','N12345',
+dt(2020, 10, 24, 14, 0), dt(2020, 10, 24, 16, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('332807','N12345',
+dt(2020, 10, 25, 16, 0), dt(2020, 10, 25, 18, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('123986','N12345',
+dt(2020, 10, 30, 14, 0), dt(2020, 10, 30, 16, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('123988','N9876A',
+dt(2020, 11, 7, 11, 0), dt(2020, 11, 7, 13, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('123989','N12345',
+dt(2020, 11, 11, 11, 0), dt(2020, 11, 11, 13, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('123991','N12345',
+dt(2020, 11, 14, 11, 0), dt(2020, 11, 14, 13, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('123992','N12345',
+dt(2020, 11, 21, 11, 0), dt(2020, 11, 21, 13, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('123993','N12345',
+dt(2020, 11, 22, 11, 0), dt(2020, 11, 22, 13, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('124311','N12345',
+dt(2020, 11, 28, 14, 0), dt(2020, 11, 28, 16, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None), 
+paperless.Schedule('124314','N12345',
+dt(2020, 11, 29, 14, 0), dt(2020, 11, 29, 16, 0),\
+'Clement, Mathieu','CfiLastName, CfiFirstName',None)]
+
         self.calendar = paperless.Calendar(self.schedules)
         self.f = tempfile.TemporaryFile()
         self.calendar.write_file(self.f)
+        self.f.seek(0) # Reset pointer to start of file before reading
         self.lines = self.f.readlines()
+        self.lines = list(map(lambda x: x[:-2].decode('UTF-8'), self.lines))
 
 
     def tearDown(self):
@@ -62,10 +77,8 @@ class TestCalendar(TestCase):
                         self.lines[2] == 'VERSION:2.0')
         self.assertEqual(self.lines.count('VERSION:2.0'), 1)
 
-        # https://en.wikipedia.org/wiki/Formal_Public_Identifier
-        prodid_pattern = 'PRODID:-//[A-Z0-9]+//.+'
-        self.assertTrue(re.match(self.lines[1], pattern) is not None or \
-                        re.match(self.lines[2], pattern) is not None)
+        self.assertTrue(self.lines[1].startswith('PRODID:') or 
+                        self.lines[2].startswith('PRODID:'))
 
 
     def test_number_of_events_matches(self):
@@ -75,6 +88,14 @@ class TestCalendar(TestCase):
         fields = ['END:VEVENT', 'DTSTART', 'DTEND', 'SUMMARY', 'CATEGORIES',
                   'DESCRIPTION', 'LOCATION', 'STATUS', 'UID']
         # TODO check check-out link for aircraft in description?
-        for field in f:
-            self.assertEqual(num_events_in_ics, self.lines.count(field), field)
- 
+        for field in fields:
+            self.assertEqual(num_events_in_ics, self.count(field), field)
+
+
+    def count(self, field):
+        field = field
+        counter = 0
+        for line in self.lines:
+            if line.startswith(field):
+                counter = counter + 1
+        return counter
